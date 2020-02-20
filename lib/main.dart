77b4844653703ed47ebc5c624560b5d8c4448257
杨磊 <youngleish@gmail.com>
2020-02-20 09:33:44 +0800

    add scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,33 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
-      routes: { // 设置路由
-        './home' : (BuildContext context) => HomePage(),
-        './detail' : (BuildContext context) => DetailPage()
-      },
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.red,
-      child: Center(
-        child: GestureDetector(
-          onTap: () { // onTabUp onTabDown
-            // print('onTab');
-            Navigator.pushNamed(context, './detail'); // 跳转页面
-          },
-          child: Text(
-            '点击跳转详情页点击跳转详情页点击跳转详情页点击跳转详情页点击跳转详情页',
-            textDirection: TextDirection.ltr, // 文本多的时候一个对齐方式问题
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontStyle: FontStyle.italic
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('首页'),
+          // leading: Text('leading'),
+          // actions: <Widget>[ // 加入action后title不居中，需要使用centerTitle
+          //   Text('action1'),
+          //   Text('action2'),
+          // ],
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text("THIS IS BODY"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.access_alarm), title: Text('闹钟')
             ),
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.accessibility), title: Text('哈哈')
+            )
+          ],
+        ),
+        drawer: Drawer(
+          elevation: 10.0,
+          child: Center(
+            child: Text('drawer'),
           ),
         ),
       ),
@@ -45,28 +44,3 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class DetailPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.blue,
-      child: Center(
-        child: GestureDetector(
-          onTap: () {
-            // Navigator.pushNamed(context, './home');
-            Navigator.pop(context); // 跳转到上一层
-            // Navigator.popAndPushNamed(context, './home'); // 先跳转到上一个页面，然后基于上一个页面进行跳转
-          },
-          child: Text(
-            "点击跳转首页",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontStyle: FontStyle.normal
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
