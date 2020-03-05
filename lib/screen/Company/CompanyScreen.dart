ba1fe012f4c465d9f126c22d3141ce9a095c7e92
杨磊 <youngleish@gmail.com>
2020-03-05 10:08:10 +0800

    upd company

--- lib/screen/Company/CompanyScreen.dart
+++ lib/screen/Company/CompanyScreen.dart
@@ -13,15 +13,29 @@ class CompanyScreen extends StatefulWidget {
 class _CompanyScreenState extends State<CompanyScreen> {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Color.fromARGB(255, 0, 215, 198),
-      child: Center(
-        child: Text(
+    return Scaffold(
+      appBar: new AppBar(
+        elevation: 0,
+        title: Text(
           '公司',
-          style: TextStyle(
-              fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),
+          style: TextStyle(fontSize: 20.0, color: Colors.white),
         ),
       ),
+      body: Center(
+        child: Text(
+          'data' 
+        )
+      )
     );
+    // return Material(
+    //   color: Color.fromARGB(255, 0, 215, 198),
+    //   child: Center(
+    //     child: Text(
+    //       '公司',
+    //       style: TextStyle(
+    //           fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),
+    //     ),
+    //   ),
+    // );
   }
 }
