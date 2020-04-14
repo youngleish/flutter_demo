dc0bb068d3a460f24d30074b4c623eaaf455bc72
杨磊 <youngleish@gmail.com>
2020-04-14 19:48:38 +0800

    通过InheritedWidget创建provider处理状态

--- lib/screen/Mine/MineMainScreen.dart
+++ lib/screen/Mine/MineMainScreen.dart
@@ -3,26 +3,83 @@ import 'package:flutter/material.dart';
 class MineScreen extends StatefulWidget {
   @override
   _MineScreen createState() => new _MineScreen();
-
-  // @override
-  // State<StatefulWidget> createState() {
-  //   // TODO: implement createState
-  //   return null;
-  // }
 }
 
 class _MineScreen extends State<MineScreen> {
+  int _count = 0;
+  void _increaseCount() {
+    setState(() {
+      _count += 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Color.fromARGB(255, 0, 215, 198),
-      child: Center(
+    return CounterProvider(
+        count: _count,
+        increaseCount: _increaseCount,
+        child: Scaffold(
+          backgroundColor: Color.fromARGB(255, 242, 242, 245),
+          appBar: AppBar(
+              title: Text(
+            '大前端',
+            style: TextStyle(fontSize: 20.0, color: Colors.white),
+          )),
+          body: MiddlerCounter(),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: _increaseCount,
+          ),
+        ));
+  }
+}
+
+class MiddlerCounter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Counter()),
+    );
+  }
+}
+
+class Counter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 获取共享数据
+    final int count = CounterProvider.of(context).count;
+    final VoidCallback increaseCount =
+        CounterProvider.of(context).increaseCount;
+    return Center(
+      child: GestureDetector(
+        onTap: increaseCount,
         child: Text(
-          '公司',
+          '$count',
           style: TextStyle(
-              fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white),
+            fontSize: 50.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.blue
+          ),
         ),
       ),
     );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  // 定义共享的数据
+  final int count;
+  final VoidCallback increaseCount;
+  final Widget child;
+  // 构造函数初始化
+  CounterProvider({this.count, this.increaseCount, this.child});
+
+  static CounterProvider of(BuildContext context) =>
+      context.dependOnInheritedWidgetOfExactType(aspect: CounterProvider);
+
+  @override
+  // 决定是否重建新的小部
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    return true;
   }
 }
